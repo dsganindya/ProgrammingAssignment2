fc744e4f77a116d6a569fad95f1acc1a298a99e2
dsganindya <52620541+dsganindya@users.noreply.github.com>
2019-08-31 17:36:39 +0530

    Update cachematrix.R
    Edited a few typos based on peer feedback. Also tried to make the code more readable.

--- cachematrix.R
+++ cachematrix.R
@@ -8,11 +8,14 @@ makeCacheMatrix <- function(matrix = matrix()) {
   #this matrix takes the input and sets the matrix and returns a list of 4 functions. this list is the special matrix. 
   #this list is also used by the other functions. 
   #inverse and matrix are values that are accessed from outside the environment
-  inverse<-NULL #we start off by setting the inverse as Null so that we can set the inverse later
+  inverse<-NULL
+  #we start off by setting the inverse as Null so that we can set the inverse later
   #now we create a set function that sets the matrix value
   set<-function(y){
-    matrix<<-y #when set function is called it sets the input matrix or updates it
-    inverse<<-NULL #obviously when the matrix value is updated the inverse has to be calculated
+    matrix<<-y 
+    #when set function is called it sets the input matrix or updates it
+    inverse<<-NULL 
+    #obviously when the matrix value is updated the inverse has to be calculated
     #again. Hence along with setting the matrix value the inverse is reset to NULL
     #Note. This function is defined here and called later. Also note that <<- sign is used to 
     #set values outside this environment
@@ -36,8 +39,9 @@ makeCacheMatrix <- function(matrix = matrix()) {
 
 cacheSolve <- function(special_matrix, ...) {
   
-        ## Return a matrix that is the inverse of 'x'
-        inverse<-special_matrix$get_inverse() #this gets the inverse from the cache
+        ## Return a matrix that is the inverse of 'special_matrix'
+        inverse<-special_matrix$get_inverse() 
+        #this gets the inverse from the cache
         if(!is.null(inverse)){
           message("Getting cached data...")
           return(inverse)
@@ -45,7 +49,8 @@ cacheSolve <- function(special_matrix, ...) {
           #from the cache
         }
         #Suppose there is no inverse value in the cache so we need to calculate it 
-        data<-special_matrix$get()#we get the matrix
+        data<-special_matrix$get()
+        #we get the matrix
         #now we create inverse
         inverse<-solve(data)
         # now we set the inverse 
